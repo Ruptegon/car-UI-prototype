752b39dc33b66d6a3fedff77765f03eb735187a6
Kacper Szkodziński <kacper.szkodzinski@outlook.com>
2022-02-03 01:48:33 +0100

    Add telltales

---
+++ CarUIPrototype/Assets/RTT/Telltale.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Telltale : MonoBehaviour
+{
+    [SerializeField]
+    private Image tellTaleOnImage;
+
+    public bool IsOn { get { return isOn; } set => SetIsOn(value); }
+    private bool isOn;
+
+    private void Start()
+    {
+        isOn = tellTaleOnImage.enabled;
+    }
+
+    private void SetIsOn(bool newIsOn) 
+    {
+        isOn = newIsOn;
+        tellTaleOnImage.enabled = isOn;
+    }
+}
